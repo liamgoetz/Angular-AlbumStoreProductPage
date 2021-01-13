f598937a29c94efd665cd6f1629cb6018136b605
liamgoetz <59585422+liamgoetz@users.noreply.github.com>
2021-01-13 15:48:37 -0500

    3

---
+++ src/app/product.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+  constructor(private _http: Http) { }
+  private class string _albumUrl = "../assets/album.json";
+
+}

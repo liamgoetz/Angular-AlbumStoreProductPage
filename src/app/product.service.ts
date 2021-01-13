abbf6058f1a67f7441f3100c42cdd7ed1634d399
liamgoetz <59585422+liamgoetz@users.noreply.github.com>
2021-01-13 15:54:01 -0500

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,5 +5,13 @@ import { Http, Response } from '@angular/http';
 export class ProductService {
   constructor(private _http: Http) { }
   
+  private _albumUrl : string = '../assets/album.json';
+  public get value() : string {
+    return this._albumUrl;
+  }
+  public set value(v : string) {
+    this._albumUrl = v;
+  }
+  
 
 }

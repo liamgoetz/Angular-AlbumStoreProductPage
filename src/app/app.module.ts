484c291f3cc2002b2998ae016862fdeed11cdd66
liamgoetz <59585422+liamgoetz@users.noreply.github.com>
2021-01-14 14:23:16 -0500

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 const appRoutes : Routes = [
   {path: 'products', component: ProductListComponent},
   {path: 'product/:id', component: ProductPageComponent},
-  {path: 'products', component: ProductListComponent}  
+  {path: '', redirectTo: 'products', pathMatch: 'full'}  
 ];
 
 

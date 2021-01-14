704b80e8da36c011a24c56f38c8a285741b01b57
liamgoetz <59585422+liamgoetz@users.noreply.github.com>
2021-01-14 11:05:40 -0500

    6

--- src/app/productng.service.ts
+++ src/app/productng.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Product } from './product';
 
 @Injectable()
 export class ProductngService {
 
+  _productsUrl : string = '../assets/products.json';
+
   constructor() { }
+
+  getProducts() {
+   
+  }
 
 }

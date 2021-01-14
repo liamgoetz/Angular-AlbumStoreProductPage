97666ec78b048b5415924e2cc2d6d3aaa54d5ef0
liamgoetz <59585422+liamgoetz@users.noreply.github.com>
2021-01-14 10:33:18 -0500

    4

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { subscribeOn } from 'rxjs/operator/subscribeOn';
+import { Album } from '../album';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  albumInfo : Album;
+
+  
+
+  constructor( private _productService : ProductService) { }
+
+  ngOnInit() {
+  }
+
+}
